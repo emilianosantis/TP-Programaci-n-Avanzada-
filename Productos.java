cf5e91c81f2482cd50a9005160e859dab4dabfc6
emilianosantis <126522374+emilianosantis@users.noreply.github.com>
2023-06-18 23:58:33 -0300

    Update Productos.java

--- Productos.java
+++ Productos.java
@@ -1,47 +1,44 @@
-package Mercado;
 
 public class Productos {
-
-    private String nombre ;
-    private int cantidad;
-    private double precio;
-    public Productos(String nombre , int cantidad , double precio){
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public int getCantidad(){
-        return cantidad;
-    }
-    public void setCantidad(int cantidad){
-        this.cantidad = cantidad;
-    }
-    public int getPrecio(){
-        return cantidad;
-    }
-    public void setPrecio(double precio){
-        this.precio = precio;
-    }
+    private String nombreDelProducto ;
+    private int cantidadDelProducto;
+    private double precioUnitario;
     public boolean stock(int numero) {
-        if (numero <= cantidad){
-            numero -= cantidad;
+        if (numero <= cantidadDelProducto){
+            numero -= cantidadDelProducto;
+            return true;
         }
         else {
             System.out.println("NO HAY MAS");
             return false;
         }
-        return true;
 
     }
+    public Productos(String nombreDelProducto , int cantidadDelProducto , double precioUnitario){
+        this.nombreDelProducto = nombreDelProducto;
+        this.cantidadDelProducto = cantidadDelProducto;
+        this.precioUnitario = precioUnitario;
+    }
+    public String getNombreDelProducto(){
+        return nombreDelProducto;
+    }
+    public void setNombreDelProducto(String nombreDelProducto) {
+        this.nombreDelProducto = nombreDelProducto;
+    }
+    public int getCantidadDelProducto(){
+        return cantidadDelProducto;
+    }
+    public void setCantidadDelProducto(int cantidad){
+        this.cantidadDelProducto = cantidad;
+    }
+    public double getPrecioUnitario() {  return precioUnitario;  }
+    public void setPrecioUnitario(double precioUnitario){
+        this.precioUnitario = precioUnitario;
+    }
+
     @Override
     public String toString(){
-        return "NOMBRE: "+ nombre + " CANTIDAD: "+ cantidad +" PRECIO DE LA UNIDAD: " + "$" + precio + "\n" ;
+        return "NOMBRE:"+ nombreDelProducto + ", CANTIDAD:" + cantidadDelProducto+ ", PRECIO DE LA UNIDAD: "+ '$' + precioUnitario;
     }
 
 }
